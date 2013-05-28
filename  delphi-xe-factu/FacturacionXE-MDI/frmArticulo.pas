028d1704e775c170f508babfc50233be36cc88cb
clauxs88 <clauxs88@gmail.com>
2013-05-28 15:55:00 +0000

    Algunos cambios y agrego una carpeta de captura desde web cam

---  delphi-xe-factu/FacturacionXE-MDI/frmArticulo.pas
+++  delphi-xe-factu/FacturacionXE-MDI/frmArticulo.pas
@@ -13,9 +13,6 @@ type
   TfrmArticuloUt = class(TfrmBaseUt)
     Panel2: TPanel;
     Button1: TButton;
-    DBGridEh1: TDBGridEh;
-    DBEdit2: TDBEdit;
-    DBEdit3: TDBEdit;
     grpArticulo: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
@@ -96,13 +93,18 @@ type
     btnEditar: TBitBtn;
     btnEliminar: TBitBtn;
     btnNuevo: TBitBtn;
+    edt1: TEdit;
+    dsGrilla: TDataSource;
+    qryGrilla: TIBQuery;
+    DBGridEh1: TDBGridEh;
+    btn1: TButton;
     procedure Image1Click(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure dsArticuloDataChange(Sender: TObject; Field: TField);
-    procedure btnEditarClick(Sender: TObject);
-    procedure btnNuevoClick(Sender: TObject);
-    procedure btnEliminarClick(Sender: TObject);
+    procedure btnAccionesClick(Sender: TObject);
+    procedure coloreo(SN: Boolean);
+    procedure btn1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -117,66 +119,119 @@ implementation
 
 {$R *.dfm}
 
-procedure TfrmArticuloUt.btnEditarClick(Sender: TObject);
+procedure TfrmArticuloUt.btn1Click(Sender: TObject);
 begin
   inherited;
-  cdsArticulo.Edit;
-  btnNuevo.Caption:='Guardar';
-  btnEliminar.Caption:='Cancelar';
-  btnEditar.Enabled:=False;
-  grpArticulo.Enabled:=True;
-end;
-
-procedure TfrmArticuloUt.btnEliminarClick(Sender: TObject);
-begin
-  inherited;
-
-  if btnEliminar.Caption='Cancelar' then
+  if btn1.Caption='Cambiar' then
    begin
-    cdsArticulo.Close;
-    cdsArticulo.Open;
-    btnEditar.Enabled:=True;
-    btnNuevo.Caption:='Nuevo';
-    btnEliminar.Caption:='Eliminar';
-   end
-  else
-  begin
-    if Utilidades.msSi('Esta Seguro Borrar el Articulo?','Borrar Articulo') = true then
-    begin
-      cdsArticulo.Delete;
-      cdsArticulo.ApplyUpdates(0);
-      cdsArticulo.Close;
-      cdsArticulo.Open;
-    end
-  end;
-  grpArticulo.Enabled:=False;
-end;
-
-procedure TfrmArticuloUt.btnNuevoClick(Sender: TObject);
-begin
-  inherited;
- if btnNuevo.Caption='Nuevo' then
-   begin
-      grpArticulo.Enabled:=True;
-      cdsArticulo.Insert;
-      qryAux.SQL.Clear;
-      qryAux.SQL.Add('select MAX(CODARTICULO)+1 FROM ARTICULO');
-      qryAux.Open;
-      cdsArticuloCODARTICULO.AsInteger:= qryAux.Fields[0].AsInteger;
-      qryAux.Close;
-      btnNuevo.Caption:='Guardar';
-      btnEditar.Enabled:=False;
-      btnEliminar.Enabled:=False;
+     dbeCodArt.Enabled:=True;
+     dbeCodArt.Enabled:=True;
+     btn1.Caption:='Ok'
    end
   else
    begin
-      cdsArticulo.ApplyUpdates(0);
-      btnNuevo.Caption:='Nuevo';
-      btnEditar.Enabled:=True;
-      btnEliminar.Enabled:=True;
-      grpArticulo.Enabled:=False;
+     dbeCodArt.Enabled:=False;
+     cdsArticuloCODARTICULO.AsInteger:=StrToInt(dbeCodArt.Text);
+     btn1.Caption:='Cambiar'
    end;
 end;
+
+procedure TfrmArticuloUt.btnAccionesClick(Sender: TObject);
+begin
+  inherited;
+  if Sender=btnNuevo then
+   begin
+    coloreo(False);
+    if btnNuevo.Caption='Nuevo' then
+     begin
+        grpArticulo.Enabled:=True;
+        cdsArticulo.Insert;
+        qryAux.SQL.Clear;
+        qryAux.SQL.Add('select MAX(CODARTICULO)+1 FROM ARTICULO');
+        qryAux.Open;
+        cdsArticuloCODARTICULO.AsInteger:= qryAux.Fields[0].AsInteger;
+        qryAux.Close;
+        btnNuevo.Caption:='Guardar';
+        btnEliminar.Caption:='Cancelar';
+        btnEditar.Enabled:=False;
+        btnEliminar.Enabled:=True;
+     end
+    else
+     begin
+        cdsArticulo.ApplyUpdates(0);
+        btnNuevo.Caption:='Nuevo';
+        btnEliminar.Caption:='Eliminar';
+        btnEditar.Enabled:=True;
+        btnEliminar.Enabled:=True;
+        grpArticulo.Enabled:=False;
+     end;
+   end;
+  ///////
+  if Sender=btnEliminar then
+  begin
+    coloreo(False);
+    if btnEliminar.Caption='Cancelar' then
+     begin
+      cdsArticulo.Close;
+      cdsArticulo.Open;
+      btnEditar.Enabled:=True;
+      btnNuevo.Caption:='Nuevo';
+      btnEliminar.Caption:='Eliminar';
+     end
+    else
+    begin
+      if Utilidades.msSi('Seguro que desea eliminar el Articulo?','Borrar Articulo') = true then
+      begin
+        cdsArticulo.Delete;
+        cdsArticulo.ApplyUpdates(0);
+        cdsArticulo.Close;
+        cdsArticulo.Open;
+      end
+    end;
+    grpArticulo.Enabled:=False;
+  end;
+  //////
+  if sender=btnEditar then
+   begin
+    cdsArticulo.Edit;
+    btnNuevo.Caption:='Guardar';
+    btnEliminar.Caption:='Cancelar';
+    btnEditar.Enabled:=False;
+    coloreo(True);
+    grpArticulo.Enabled:=True;
+   end;
+
+end;
+
+procedure TfrmArticuloUt.coloreo(SN: Boolean);
+   begin
+     if SN=True then
+      begin
+       shp1.Brush.Color:=$009EA4FE;
+       dbeDescArt.Color:=$009EA4FE;
+       dblUnidadMedida.Color:=$009EA4FE;
+       dblTipoIva.Color:=$009EA4FE;
+       dblRamo.Color:=$009EA4FE;
+       dblProveedor.Color:=$009EA4FE;
+       dbedtPRECIOVTA.Color:=$009EA4FE;
+       dbedtPRECIOCOMPRA.Color:=$009EA4FE;
+       dbedtEXISTENCIA.Color:=$009EA4FE;
+       dbedtEXISTENCIA_MIN.Color:=$009EA4FE;
+      end
+     else
+      begin
+       shp1.Brush.Color:=clWhite;
+        dbeDescArt.Color:=clWhite;
+       dblUnidadMedida.Color:=clWhite;
+       dblTipoIva.Color:=clWhite;
+       dblRamo.Color:=clWhite;
+       dblProveedor.Color:=clWhite;
+       dbedtPRECIOVTA.Color:=clWhite;
+       dbedtPRECIOCOMPRA.Color:=clWhite;
+       dbedtEXISTENCIA.Color:=clWhite;
+       dbedtEXISTENCIA_MIN.Color:=clWhite;
+      end;
+   end;
 
 procedure TfrmArticuloUt.dsArticuloDataChange(Sender: TObject; Field: TField);
 var
@@ -219,6 +274,7 @@ begin
   qryTipoIva.Active:=true;
   qryRamo.Active:=true;
   qryProveedor.Active:=true;
+  PageControl1.ActivePageIndex:=0;
 end;
 
 procedure TfrmArticuloUt.Image1Click(Sender: TObject);
